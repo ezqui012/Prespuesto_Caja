783d41e1c45c771519f018deaefceaba20267d92
Rylmar Quispe <rylm.fqt28@gmail.com>
2021-05-25 12:46:46 -0400

    Implementacion de Modelo y captura de datos del activity CompanyInformation

--- app/src/main/java/com/example/proyecto_flujo_caja/CompanyInformation.java
+++ app/src/main/java/com/example/proyecto_flujo_caja/CompanyInformation.java
@@ -3,12 +3,32 @@ package com.example.proyecto_flujo_caja;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.EditText;
+
+import com.example.proyecto_flujo_caja.Models.Company;
 
 public class CompanyInformation extends AppCompatActivity {
+
+    private Company company;
+
+    private EditText input_sales;
+    private EditText input_credit30;
+    private EditText input_credit60;
+    private EditText input_about;
+    private EditText input_badDebt;
+    private EditText input_interest;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_company_information);
+
+        input_sales = (EditText) findViewById(R.id.input_sales);
+        input_credit30 = (EditText) findViewById(R.id.input_credit30);
+        input_credit60 = (EditText) findViewById(R.id.input_credit60);
+        input_about = (EditText) findViewById(R.id.input_about);
+        input_badDebt = (EditText) findViewById(R.id.input_bad_debt);
+        input_interest = (EditText) findViewById(R.id.input_interest);
+
     }
 }

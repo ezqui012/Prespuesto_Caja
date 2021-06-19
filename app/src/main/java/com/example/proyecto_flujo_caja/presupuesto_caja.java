fb508e96a1b5a50a44587fbe29e0ffef013c76ce
Fernanda <ferchiea2@gmail.com>
2021-06-18 22:09:07 -0400

    6/18/21 Presupuesto and buy

---
+++ app/src/main/java/com/example/proyecto_flujo_caja/presupuesto_caja.java
@@ -0,0 +1,27 @@
+package com.example.proyecto_flujo_caja;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.text.TextUtils;
+import android.view.View;
+import android.widget.*;
+import android.widget.Button;
+
+
+
+
+import android.os.*;
+
+import androidx.appcompat.app.*;
+
+public class presupuesto_caja extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_presupuesto_caja);
+    }
+
+}
